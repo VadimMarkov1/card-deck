3fb9fce766fc80b56ab396815e88ea4011bf2f0d
Vadym Markov <trezub123456@gmail.com>
2025-08-10 13:58:00 +0300

    GHpages

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        publicPath: '/',
+        publicPath: '/card-deck/',
     },
     plugins: [
         new CleanWebpackPlugin(),
