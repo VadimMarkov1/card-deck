ee9164160fda1ae76000bdb4eaf9fa88f47b7397
Vadym Markov <trezub123456@gmail.com>
2025-08-10 14:12:43 +0300

    GHpages

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { App} from "./core/App";
+declare let __webpack_public_path__: string;
+__webpack_public_path__ = process.env.NODE_ENV === 'production' ? '/card-deck/' : '/';
+
 
 new App().init();
